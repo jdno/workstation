facffc189a453ea8563bd00d745c3cc74d1153f5
Jan David <jdno@jdno.dev>
2022-07-29 09:19:27 +0200

    Switch to iterm2

--- roles/git/files/graphite.zsh
+++ roles/git/files/graphite.zsh
@@ -11,8 +11,8 @@
 _gt_yargs_completions() {
 	local reply
 	local si=$IFS
-	IFS=$''
-	reply=($(COMP_CWORD="$((CURRENT - 1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /opt/homebrew/bin/gt --get-yargs-completions "${words[@]}"))
+	IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT - 1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" /opt/homebrew/bin/gt --get-yargs-completions "${words[@]}"))
 	IFS=$si
 	_describe 'values' reply
 }
